665efb9268f4d4e8530a22117f6ae4f18728f609
Mark Khil <marikya50@gmail.com>
2025-07-29 21:59:04 +1000

    Create SheetsRepository, implement data fetching from API and Resolve duplicate META-INF/INDEX.LIST packaging issue, Exclude duplicate DEPENDENCIES file

--- app/src/main/java/com/example/a1gworkapp/MainActivity.kt
+++ app/src/main/java/com/example/a1gworkapp/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.a1gworkapp.ui.home.HomeScreen
+import com.example.a1gworkapp.ui.login.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            LoginScreen()
         }
     }
 }
