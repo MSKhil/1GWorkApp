38595863ae5ea0936079863e7dfef7b8b0f9ddca
Mark Khil <marikya50@gmail.com>
2025-08-18 22:39:11 +1000

    Implement chained dropdowns and state hoisting for LoginScreen

--- app/src/main/java/com/example/a1gworkapp/MainActivity.kt
+++ app/src/main/java/com/example/a1gworkapp/MainActivity.kt
@@ -1,16 +1,110 @@
 package com.example.a1gworkapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.a1gworkapp.ui.home.HomeScreen
 import com.example.a1gworkapp.ui.login.LoginScreen
+import androidx.compose.runtime.remember
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            LoginScreen()
+            data class UserData(val city: String, val shop: String, val employee: String)
+
+            val allUsers = listOf(
+                UserData("Владивосток", "Седанка сити", "Хиль М"),
+                UserData("Владивосток", "Седанка сити", "Кондратюк А"),
+                UserData("Владивосток", "Седанка сити", "Мигалев А"),
+                UserData("Владивосток", "Седанка сити", "Калиновский Р"),
+                UserData("Владивосток", "Калина Молл", "Древетняк В"),
+                UserData("Владивосток", "Калина Молл", "Олешко М"),
+                UserData("Владивосток", "Калина Молл", "Филатов Н"),
+                UserData("Владивосток", "Калина Молл", "Бобрышев К"),
+                UserData("Владивосток", "Калина Молл", "Строев И"),
+                UserData("Владивосток", "Светланская", "Петров А"),
+                UserData("Владивосток", "Светланская", "Ким Д"),
+                UserData("Владивосток", "Светланская", "Гаврыш В"),
+                UserData("Находка", "Мега", "Дегтярев А"),
+                UserData("Находка", "Мега", "Дегтярева В"),
+                UserData("Уссурийск", "Тимирязева", "Каткина А"),
+                UserData("Уссурийск", "Тимирязева", "Катане Д"),
+            )
+
+            var selectedCity by remember { mutableStateOf("") }
+            var selectedShop by remember { mutableStateOf("") }
+            var selectedEmployee by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+            val cities = remember { allUsers.map { it.city }.distinct() }
+            Log.d("MainActivity", "Список городов: $cities")
+            val shops = remember(selectedCity){
+                derivedStateOf {
+                    if (selectedCity.isNotBlank()) {
+                        allUsers
+                            .filter { it.city == selectedCity }
+                            .map { it.shop }
+                            .distinct()
+                    } else {
+                        emptyList()
+                    }
+                }
+            }.value
+            val employees = remember(selectedShop){
+                derivedStateOf {
+                    if (selectedShop.isNotBlank()) {
+                        allUsers
+                            .filter { it.shop == selectedShop }
+                            .map { it.employee }
+                            .distinct()
+                    } else{
+                        emptyList()
+                    }
+                }
+            }.value
+            var isCityMenuExpanded by remember { mutableStateOf(false) }
+            var isShopMenuExpanded by remember { mutableStateOf(false) }
+            var isEmployeeMenuExpanded by remember { mutableStateOf(false) }
+
+            LaunchedEffect(key1 = selectedCity) {
+                selectedShop = ""
+                selectedEmployee = ""
+            }
+            LaunchedEffect(key1 = selectedShop) {
+                selectedEmployee = ""
+            }
+            LoginScreen(
+                cities = cities,
+                shops = shops,
+                employees = employees,
+                selectedCity = selectedCity,
+                selectedShop = selectedShop,
+                selectedEmployee = selectedEmployee,
+                passwordValue = password,
+                isCityMenuExpanded = isCityMenuExpanded,
+                isShopMenuExpanded = isShopMenuExpanded,
+                isEmployeeMenuExpanded = isEmployeeMenuExpanded,
+                onCityMenuExpandedChange = { isCityMenuExpanded = it },
+                onShopMenuExpandedChange = { isShopMenuExpanded = it },
+                onEmployeeMenuExpandedChange = { isEmployeeMenuExpanded = it },
+                onCitySelected = { city -> selectedCity = city },
+                onShopSelected = { shop -> selectedShop = shop },
+                onEmployeeSelected = { employee -> selectedEmployee = employee },
+                onPasswordChange = { newPassword -> password = newPassword },
+                onLoginClick = {
+
+                }
+            )
         }
     }
 }
--- app/src/main/java/com/example/a1gworkapp/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/a1gworkapp/ui/login/LoginScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -28,64 +29,28 @@ import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-data class UserData(val city: String, val shop: String, val employee: String)
-
-val allUsers = listOf(
-    UserData("Владивосток", "Седанка сити", "Хиль М"),
-    UserData("Владивосток", "Седанка сити", "Кондратюк А"),
-    UserData("Владивосток", "Седанка сити", "Мигалев А"),
-    UserData("Владивосток", "Седанка сити", "Калиновский Р"),
-    UserData("Владивосток", "Калина Молл", "Древетняк В"),
-    UserData("Владивосток", "Калина Молл", "Олешко М"),
-    UserData("Владивосток", "Калина Молл", "Филатов Н"),
-    UserData("Владивосток", "Калина Молл", "Бобрышев К"),
-    UserData("Владивосток", "Калина Молл", "Строев И"),
-    UserData("Владивосток", "Светланская", "Петров А"),
-    UserData("Владивосток", "Светланская", "Ким Д"),
-    UserData("Владивосток", "Светланская", "Гаврыш В"),
-    UserData("Находка", "Мега", "Дегтярев А"),
-    UserData("Находка", "Мега", "Дегтярева В"),
-    UserData("Уссурийск", "Тимирязева", "Каткина А"),
-    UserData("Уссурийск", "Тимирязева", "Катане Д"),
-)
-
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
-fun LoginScreen() {
-    var selectedCity by remember { mutableStateOf("") }
-    var selectedShop by remember { mutableStateOf("") }
-    var selectedEmployee by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    val cities = remember { allUsers.map { it.city }.distinct() }
-    val shops = remember(selectedCity){
-        derivedStateOf {
-            if (selectedCity.isNotBlank()) {
-                allUsers
-                    .filter { it.city == selectedCity }
-                    .map { it.shop }
-                    .distinct()
-            } else {
-                emptyList()
-            }
-        }
-    }.value
-    val employees = remember(selectedShop){
-        derivedStateOf {
-            if (selectedShop.isNotBlank()) {
-                allUsers
-                    .filter { it.shop == selectedShop }
-                    .map { it.employee }
-                    .distinct()
-            } else{
-                emptyList()
-            }
-        }
-    }.value
-    var isCityMenuExpended by remember { mutableStateOf(false) }
-    var isShopMenuExpended by remember { mutableStateOf(false) }
-    var isEmployeeMenuExpended by remember { mutableStateOf(false) }
-
+fun LoginScreen(
+    cities: List<String>,
+    shops: List<String>,
+    employees: List<String>,
+    selectedCity: String,
+    selectedShop: String,
+    selectedEmployee: String,
+    passwordValue: String,
+    isCityMenuExpanded: Boolean,
+    isShopMenuExpanded: Boolean,
+    isEmployeeMenuExpanded: Boolean,
+    onCityMenuExpandedChange: (Boolean) -> Unit,
+    onShopMenuExpandedChange: (Boolean) -> Unit,
+    onEmployeeMenuExpandedChange: (Boolean) -> Unit,
+    onCitySelected: (String) -> Unit,
+    onShopSelected: (String) -> Unit,
+    onEmployeeSelected: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onLoginClick: () -> Unit
+) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -93,8 +58,8 @@ fun LoginScreen() {
     ){
 //Меню выбора города
         ExposedDropdownMenuBox (
-            expanded = isCityMenuExpended,
-            onExpandedChange = { isCityMenuExpended = it }
+            expanded = isCityMenuExpanded,
+            onExpandedChange = { onCityMenuExpandedChange(it) }
         ){
             TextField(
                 modifier = Modifier.menuAnchor(),
@@ -102,33 +67,29 @@ fun LoginScreen() {
                 value = selectedCity,
                 onValueChange = {},
                 label = { Text("Город") },
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isCityMenuExpended) }
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isCityMenuExpanded) }
             )
 
             ExposedDropdownMenu(
-                expanded = isCityMenuExpended,
-                onDismissRequest = { isCityMenuExpended = false }
+                expanded = isCityMenuExpanded,
+                onDismissRequest = { onCityMenuExpandedChange(false) }
             ){
                 cities.forEach { city ->
                     DropdownMenuItem(
                         text = { Text(text = city) },
                         onClick = {
-                            selectedCity = city
-                            isCityMenuExpended = false
+                            onCitySelected(city)
+                            onCityMenuExpandedChange(false)
                         }
                     )
                 }
             }
         }
         Spacer(modifier = Modifier.height(8.dp))
-        LaunchedEffect(key1 = selectedCity) {
-            selectedShop = ""
-            selectedEmployee = ""
-        }
 //Меню выбора магазина
         ExposedDropdownMenuBox(
-            expanded = isShopMenuExpended,
-            onExpandedChange = { isShopMenuExpended = it }
+            expanded = isShopMenuExpanded,
+            onExpandedChange = { onShopMenuExpandedChange(it) }
         ) {
             TextField(
                 modifier = Modifier.menuAnchor(),
@@ -136,32 +97,29 @@ fun LoginScreen() {
                 value = selectedShop,
                 onValueChange = {},
                 label = { Text("Магазин") },
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isShopMenuExpended) }
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isShopMenuExpanded) }
             )
 
             ExposedDropdownMenu(
-                expanded = isShopMenuExpended,
-                onDismissRequest = { isShopMenuExpended = false }
+                expanded = isShopMenuExpanded,
+                onDismissRequest = { onShopMenuExpandedChange(false) }
             ) {
                 shops.forEach { shop ->
                     DropdownMenuItem(
                         text = { Text(text = shop) },
                         onClick = {
-                            selectedShop = shop
-                            isShopMenuExpended = false
+                            onShopSelected(shop)
+                            onShopMenuExpandedChange(false)
                         }
                     )
                 }
             }
         }
         Spacer(modifier = Modifier.height(8.dp))
-        LaunchedEffect(key1 = selectedShop) {
-            selectedEmployee = ""
-        }
 //Меню выбора сотрудника
         ExposedDropdownMenuBox(
-            expanded = isEmployeeMenuExpended,
-            onExpandedChange = { isEmployeeMenuExpended = it }
+            expanded = isEmployeeMenuExpanded,
+            onExpandedChange = { onEmployeeMenuExpandedChange(it) }
         ) {
             TextField(
                 modifier = Modifier.menuAnchor(),
@@ -169,19 +127,19 @@ fun LoginScreen() {
                 value = selectedEmployee,
                 onValueChange = {},
                 label = { Text("Сотрудник") },
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isEmployeeMenuExpended) }
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isEmployeeMenuExpanded) }
             )
 
             ExposedDropdownMenu(
-                expanded = isEmployeeMenuExpended,
-                onDismissRequest = { isEmployeeMenuExpended = false }
+                expanded = isEmployeeMenuExpanded,
+                onDismissRequest = { onEmployeeMenuExpandedChange(false) }
             ) {
                 employees.forEach { employee ->
                     DropdownMenuItem(
                         text = { Text(text = employee) },
                         onClick = {
-                            selectedEmployee = employee
-                            isEmployeeMenuExpended = false
+                            onEmployeeSelected(employee)
+                            onEmployeeMenuExpandedChange(false)
                         }
                     )
                 }
@@ -190,8 +148,8 @@ fun LoginScreen() {
         Spacer(modifier = Modifier.height(16.dp))
 
         TextField(
-            value = password,
-            onValueChange = { password = it },
+            value = passwordValue,
+            onValueChange = { onPasswordChange(it) },
             label = { Text("Пароль") }
         )
         Spacer(modifier = Modifier.height(16.dp))
